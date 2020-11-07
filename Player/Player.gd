59d2f11b072974af8c882656c8fa5a5e755ed180
sleepypanda <cheeyang85@gmail.com>
2020-11-07 18:37:26 +0800

    changed watermelon collision bit

--- Player/Player.gd
+++ Player/Player.gd
@@ -39,6 +39,7 @@ var leftbutton = "Left_A"
 var rightbutton = "Right_A"
 var jumpbutton = "Jump_A"
 signal rangeAttack(playerid)
+signal hitwatermelon(playerid, watermelon)
 
 onready var dashtimer = $DashInputTimer
 onready var dashdurationtimer = $DashDurationTimer
@@ -377,4 +378,5 @@ func _on_BlinkTimer_timeout():
 func _on_Hitbox_body_entered(body):
 	if body.is_in_group("bullet"):
 		body.linear_velocity = Vector2(direction * 600, -300)
-		print("got hit bullet")
+		emit_signal("hitwatermelon", name, body)
+		
